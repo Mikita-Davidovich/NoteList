fba807ccacf9f6230ad41df5d8d4cbeb443607b1
Mikita-Davidovich <tosblrodo@gmail.com>
2021-09-27 11:54:39 +0300

    feat: update README.md, change homepage, establish basename, add path 'PATH_ROOT'

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import About from './pages/about';
 import SharedNotes from './pages/sharedNotes';
 import PageNotFound from './pages/pageNotFound';
 import NavBar from './shared/navBar';
-import { PATH_MY_NOTES, PATH_SHARED_NOTES, PATH_ABOUT, PATH_NOT_FOUND } from './assets/paths';
+import { PATH_MY_NOTES, PATH_SHARED_NOTES, PATH_ABOUT, PATH_NOT_FOUND, PATH_ROOT } from './assets/paths';
 import theme from './theme';
 
 const App = () => {
@@ -16,8 +16,11 @@ const App = () => {
   };
   return (
     <MuiThemeProvider theme={theme}>
-      <BrowserRouter>
+      <BrowserRouter basename="/Notes-List">
         <Switch>
+          <Route exact path={PATH_ROOT}>
+            <Redirect to={PATH_MY_NOTES} />
+          </Route>
           <Route path={PATH_MY_NOTES} exact component={() => <MyNotes isNavBarOpen={isNavBarOpen} openNavBar={openNavBar} />} />
           <Route path={PATH_ABOUT} component={() => <About isNavBarOpen={isNavBarOpen} openNavBar={openNavBar} />} />
           <Route path={PATH_SHARED_NOTES} component={() => <SharedNotes isNavBarOpen={isNavBarOpen} openNavBar={openNavBar} />} />
